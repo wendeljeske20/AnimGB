e59ced2a0b9341b5516cec9b18cea289b4364df5
Wendel <wendelblankjeske@gmail.com>
2019-11-26 22:25:21 -0300

    sadas

---
+++ Assets/CameraTarget.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+	public GameObject target;
+	private void Update()
+	{
+		transform.position = target.transform.position;
+	}
+}
